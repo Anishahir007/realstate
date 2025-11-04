5a277d33c42c96bb163ce93d0becf5c4485c6007
Anish Yadav <144926668+Anishahir007@users.noreply.github.com>
2025-11-04 17:23:35 +0530

    Update PropertyCard.jsx

--- frontend/src/superadmin/templates/proclassic/components/property/PropertyCard.jsx
+++ frontend/src/superadmin/templates/proclassic/components/property/PropertyCard.jsx
@@ -13,10 +13,29 @@ function formatPriceINR(num) {
 
 export default function PropertyCard({ property }) {
   const { slug } = useParams();
+  
+  // Console log property data for debugging
+  console.log('PropertyCard - Full property data:', property);
+  console.log('PropertyCard - Image fields:', {
+    image_url: property.image_url,
+    image: property.image,
+    primary_image: property.primary_image,
+    media: property.media
+  });
+  
   const to = `property/${property.id}`; // relative path works for both domain root and /site/:slug
   let img = undefined;
   if (property.image_url) img = property.image_url;
   else if (property.image) img = property.image;
+  else if (property.primary_image) img = property.primary_image;
+  else if (property.media && Array.isArray(property.media) && property.media.length > 0) {
+    // Try to get first image from media array
+    const firstMedia = property.media.find(m => m.file_url || m.url);
+    if (firstMedia) img = firstMedia.file_url || firstMedia.url;
+  }
+  
+  console.log('PropertyCard - Final image URL:', img);
+  
   const price = formatPriceINR(property.expected_price);
   const size = property.built_up_area ? `${property.built_up_area} ${property.area_unit || ''}` : '';
   const type = property.property_type || 'Property';
