6231c68cc419359e059087dcc8edd0aed0d0166e
Anish Yadav <144926668+Anishahir007@users.noreply.github.com>
2025-11-04 18:00:38 +0530

    build property card

--- frontend/src/superadmin/templates/proclassic/components/property/PropertyCard.jsx
+++ frontend/src/superadmin/templates/proclassic/components/property/PropertyCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { getApiBase } from '../../../../utils/apiBase.js';
+import { getApiBase } from '../../../../../utils/apiBase.js';
 import './property.css';
 
 function formatPriceINR(num) {
